5ed1b3709eee7d91c4fd98611ba6c99f53ac4783
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2025-11-17 15:12:32 -0800

    Implement Auth UI and Layout

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useForm, SubmitHandler } from "react-hook-form";
+import InputField from "@/components/forms/InputField";
+import SelectField from "@/components/forms/SelectField";
+import {
+  INVESTMENT_GOALS,
+  PREFERRED_INDUSTRIES,
+  RISK_TOLERANCE_OPTIONS,
+} from "@/lib/constants";
+import { CountrySelectField } from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
+
+const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      country: "",
+      investmentGoals: "Growth",
+      riskTolerance: "High",
+      preferredIndustry: "Technology",
+    },
+    mode: "onBlur",
+  });
+  const onSubmit: SubmitHandler<SignUpFormData> = (data: SignUpFormData) => {
+    try {
+      console.log("Sign-up data:", data);
+    } catch (error) {
+      console.error("Sign-up error:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="form-title">Sign Up & Personalize</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField
+          name="fullName"
+          label="Full Name"
+          placeholder="Enter your full name"
+          register={register}
+          error={errors.fullName}
+          validation={{ required: "Full name is required", minLength: 2 }}
+        />
+
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="Enter your email"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email is required",
+            pattern: /^\S+@\S+$/i,
+            message: "Invalid email address",
+          }}
+        />
+
+        <InputField
+          name="password"
+          label="Password"
+          placeholder="Enter a strong Password"
+          type="password"
+          register={register}
+          error={errors.password}
+          validation={{ required: "Password is required", minLength: 10 }}
+        />
+
+        <CountrySelectField
+          name="country"
+          label="Country"
+          control={control}
+          error={errors.country}
+          required
+        />
+
+        <SelectField
+          name="investmentGoals"
+          label="Investment Goals"
+          placeholder="Select your investment goals"
+          options={INVESTMENT_GOALS}
+          control={control}
+          error={errors.investmentGoals}
+          required
+        />
+
+        <SelectField
+          name="riskTolerance"
+          label="Risk Tolerance"
+          placeholder="Select your risk level"
+          options={RISK_TOLERANCE_OPTIONS}
+          control={control}
+          error={errors.riskTolerance}
+          required
+        />
+
+        <SelectField
+          name="prefferedIndustry"
+          label="Preferred Industry"
+          placeholder="Select your preffered industry"
+          options={PREFERRED_INDUSTRIES}
+          control={control}
+          error={errors.preferredIndustry}
+          required
+        />
+
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting
+            ? "Creating Account..."
+            : "Start Your Investing Journey"}
+        </Button>
+
+        <FooterLink
+          text="Already have an account?"
+          linkText="Sign In"
+          href="/sign-in"
+        />
+      </form>
+    </>
+  );
+};
+
+export default SignUp;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="min-h-screen text-gray-500">
+    <main className="min-h-screen text-gray-400">
       <Header />
       <div className="container py-10">{children}</div>
     </main>
